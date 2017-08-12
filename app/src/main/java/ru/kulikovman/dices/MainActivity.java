d597b1ca8ddbf198c791a2db016b44659970d691
Maksim Kulikov <kulikovman@gmail.com>
2017-08-12 11:44:59 +0500

    Причесали код

--- app/src/main/java/ru/kulikovman/dices/MainActivity.java
+++ app/src/main/java/ru/kulikovman/dices/MainActivity.java
@@ -17,12 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private ImageView mLeftDice, mRightDice;
     private int mLeftNumber, mRightNumber, mLeftView, mRightView;
 
-    public static final String PREFERENCES = "dice_settings";
-    public static final String LEFT_DICE_NUMBER = "left_dice_number";
-    public static final String RIGHT_DICE_NUMBER = "right_dice_number";
-    public static final String LEFT_DICE_VIEW = "left_dice_view";
-    public static final String RIGHT_DICE_VIEW = "right_dice_view";
-    private SharedPreferences mSettings;
     private SharedPreferences mSharedPref;
 
     @Override
@@ -30,30 +24,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Инициализируем наши ImageView с кубиками
         mRightDice = (ImageView) findViewById(R.id.dice_right);
         mLeftDice = (ImageView) findViewById(R.id.dice_left);
-
-        /*// Получаем SharedPreferences
-        mSettings = getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
-
-        // Если сохранены значения и вид кубиков, по получаем их
-        if (mSettings.contains(LEFT_DICE_NUMBER)
-                //&& mSettings.contains(RIGHT_DICE_NUMBER) &&
-                //mSettings.contains(LEFT_DICE_VIEW) && mSettings.contains(RIGHT_DICE_VIEW)
-                ) {
-
-            // Восттанавливаем значения переменных
-            mLeftNumber = mSettings.getInt(LEFT_DICE_NUMBER, 1);
-            mRightNumber = mSettings.getInt(RIGHT_DICE_NUMBER, 1);
-            mLeftView = mSettings.getInt(LEFT_DICE_VIEW, 1);
-            mRightView = mSettings.getInt(RIGHT_DICE_VIEW, 1);
-
-            // Загружаем картинки кубиков
-            loadDiceImage(mLeftDice, mLeftNumber, mLeftView);
-            loadDiceImage(mRightDice, mRightNumber, mRightView);
-            Log.d("myLog", "Восстановили значения и вид кубиков");
-        }*/
-
 
         // Получаем SharedPreferences
         mSharedPref = getPreferences(Context.MODE_PRIVATE);
@@ -74,20 +47,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
 
-        // Создаем SharedPreferences и если переменные не пустые, то сохраняем их
-        /*SharedPreferences.Editor editor = mSettings.edit();
-        if (mLeftNumber != 0 && mRightNumber != 0) {
-            editor.putInt(LEFT_DICE_NUMBER, mLeftNumber);
-            editor.putInt(RIGHT_DICE_NUMBER, mRightNumber);
-            Log.d("myLog", "Сохранили значения кубиков: " + mLeftNumber + " | " + mRightNumber);
-        }
-        if (mLeftView != 0 && mRightView != 0) {
-            editor.putInt(LEFT_DICE_VIEW, mLeftView);
-            editor.putInt(RIGHT_DICE_VIEW, mRightView);
-            Log.d("myLog", "Сохранили вид кубиков: " + mLeftView + " | " + mRightView);
-        }
-        editor.apply();*/
-
+        // Сохраняем значения кубиков, если они не равны нулю
         if (mLeftNumber != 0 && mRightNumber != 0 && mLeftView != 0 && mRightView != 0) {
             mSharedPref = getPreferences(Context.MODE_PRIVATE);
 
