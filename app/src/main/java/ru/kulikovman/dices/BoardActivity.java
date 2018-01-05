2f8f7b0458b9de3556d0f1769899956a61051691
Maksim Kulikov <kulikovman@gmail.com>
2018-01-05 21:49:02 +0500

    Размещение одного кубика в случайных координатах

--- app/src/main/java/ru/kulikovman/dices/BoardActivity.java
+++ app/src/main/java/ru/kulikovman/dices/BoardActivity.java
@@ -1,41 +1,98 @@
 package ru.kulikovman.dices;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ViewFlipper;
+import android.view.ViewGroup;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Random;
 
 public class BoardActivity extends AppCompatActivity {
 
-    private ViewFlipper mFlipper;
-    private Button mButton1, mButton2, mButton3, mButton4;
+    //private ViewFlipper mFlipper;
+    //private Button mButton1, mButton2, mButton3, mButton4;
+    private LinearLayout mBoard;
+    private ImageView mDiceOne;
+    private int mWidth, mHeight, mX, mY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_board);
+        setContentView(R.layout.game_board);
 
-        mFlipper = findViewById(R.id.cubes_container);
-        mButton1 = findViewById(R.id.button_1);
+        Log.d("log", "Запущен onCreate в BoardActivity");
+
+        //mFlipper = findViewById(R.id.cubes_container);
+        /*mButton1 = findViewById(R.id.button_1);
         mButton2 = findViewById(R.id.button_2);
         mButton3 = findViewById(R.id.button_3);
-        mButton4 = findViewById(R.id.button_4);
+        mButton4 = findViewById(R.id.button_4);*/
 
-        Canvas canvas = new Canvas();
-        Paint paint = new Paint();
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice_1_01);
+        mBoard = findViewById(R.id.board);
+        mDiceOne = findViewById(R.id.dice_1);
 
-        canvas.drawBitmap(bitmap, 100, 100, paint);
+
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        mWidth = displayMetrics.widthPixels - convertDpToPx(140);
+        mHeight = displayMetrics.heightPixels - convertDpToPx(140 + 100);
+
+        moveToRandomPosition(mDiceOne);
+
+        Log.d("log", "Размер: " + mWidth + " x " + mHeight);
+
 
     }
 
+    private void moveToRandomPosition(ImageView dice) {
+        // Получаем случайные координаты кубика
+        Random random = new Random();
+        mX = 1 + random.nextInt(mWidth);
+        mY = 1 + random.nextInt(mHeight);
 
-    public void selectNumberCubes(View view) {
+        Log.d("log", "Координаты: " + mX + " x " + mY);
+
+        setMargin(dice, mX, mY);
+    }
+
+    public int convertDpToPx(int valueInDp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, valueInDp,
+                getResources().getDisplayMetrics());
+    }
+
+    private void setMargin(View view, int x, int y) {
+        MarginLayoutParams params = (MarginLayoutParams) view.getLayoutParams();
+        params.topMargin = y;
+        params.leftMargin = x;
+        view.setLayoutParams(params);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("log", "Запущен onResume в BoardActivity");
+
+        /*mWidth = mBoard.getWidth();
+        mHeight = mBoard.getHeight();
+
+        Log.d("log", "Размер: " + mWidth + " x " + mHeight);*/
+    }
+
+    public void dropDice(View view) {
+        moveToRandomPosition(mDiceOne);
+    }
+
+    /*public void selectNumberCubes(View view) {
         int id = view.getId();
         selectButton(view);
 
@@ -53,16 +110,16 @@ public class BoardActivity extends AppCompatActivity {
                 mFlipper.setDisplayedChild(3);
                 break;
         }
-    }
+    }*/
 
-    private void selectButton(View view) {
+    /*private void selectButton(View view) {
         mButton1.setSelected(false);
         mButton2.setSelected(false);
         mButton3.setSelected(false);
         mButton4.setSelected(false);
 
         view.setSelected(true);
-    }
+    }*/
 
 
 }
