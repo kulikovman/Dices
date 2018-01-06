52434b0da3aed8838503dd1e12ed784e0368bafc
Maksim Kulikov <kulikovman@gmail.com>
2018-01-07 02:59:25 +0500

    Оптимизация кода

--- app/src/main/java/ru/kulikovman/dices/BoardActivity.java
+++ app/src/main/java/ru/kulikovman/dices/BoardActivity.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
@@ -18,9 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup.MarginLayoutParams;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,7 +27,6 @@ import ru.kulikovman.dices.models.Dice;
 public class BoardActivity extends AppCompatActivity {
 
     private Button mButton1, mButton2, mButton3, mButton4;
-    private FrameLayout mBoard;
     private ImageView mDice1, mDice2, mDice3, mDice4;
     private int mWidth, mHeight;
 
@@ -54,7 +49,6 @@ public class BoardActivity extends AppCompatActivity {
         Log.d("log", "Запущен onCreate в BoardActivity");
 
         // Инициализируем вью элементы
-        mBoard = findViewById(R.id.board);
         mButton1 = findViewById(R.id.button_1);
         mButton2 = findViewById(R.id.button_2);
         mButton3 = findViewById(R.id.button_3);
@@ -71,6 +65,9 @@ public class BoardActivity extends AppCompatActivity {
         // Создаем SoundPool
         createSoundPool();
 
+        // Инициализация ShakeDetector
+        initShakeDetector();
+
         // Получение границ координат для размещения кубиков
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -81,32 +78,6 @@ public class BoardActivity extends AppCompatActivity {
         selectButton(mNumber);
         prepareBoard(mNumber);
         dropDices(false);
-
-        // ShakeDetector initialization
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (mSensorManager != null) {
-            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        }
-
-        mShakeDetector = new ShakeDetector();
-        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
-
-            @Override
-            public void onShake(int count) {
-				/*
-				 * The following method, "handleShakeEvent(count):" is a stub //
-				 * method you would use to setup whatever you want done once the
-				 * device has been shook.
-				 */
-
-                Log.d("log", "Обнаружена тряска - " + count);
-                //handleShakeEvent(count);
-            }
-        });
-    }
-
-    private void handleShakeEvent(int count) {
-
     }
 
     @Override
@@ -137,6 +108,137 @@ public class BoardActivity extends AppCompatActivity {
 
         // Add the following line to register the Session Manager Listener onResume
         mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    private void initShakeDetector() {
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager != null) {
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+				/*
+				 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.
+				 */
+                dropDices(true);
+                Log.d("log", "Обнаружена тряска - " + count);
+            }
+        });
+    }
+
+    @SuppressWarnings("deprecation")
+    private void createSoundPool() {
+        if (mSoundPool == null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                // Создаем SoundPool для Android API 21 и выше
+                AudioAttributes attributes = new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_GAME)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                        .build();
+
+                mSoundPool = new SoundPool.Builder()
+                        .setAudioAttributes(attributes)
+                        .build();
+            } else {
+                // Создаем SoundPool для старых версий Android
+                mSoundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+            }
+
+            Log.d("myLog", "Создали SoundPool");
+
+            // Получаем id звуковых файлов
+            loadIdSounds();
+        }
+    }
+
+    private void loadIdSounds() {
+        mRollDiceSound = mSoundPool.load(this, R.raw.roll_dice, 1);
+
+        Log.d("myLog", "Получили id звуковых файлов");
+    }
+
+    private void clearSoundPool() {
+        mSoundPool.release();
+        mSoundPool = null;
+
+        Log.d("myLog", "SoundPool очищен");
+    }
+
+    public int getStatusBarHeight() {
+        // Получение высоты статус-бара
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight;
+    }
+
+    private void prepareBoard(int number) {
+        // Сначала скрываем все кубики
+        mDice2.setVisibility(View.INVISIBLE);
+        mDice3.setVisibility(View.INVISIBLE);
+        mDice4.setVisibility(View.INVISIBLE);
+
+        // Показываем нужное количество кубиков
+        if (number >= 2) {
+            mDice2.setVisibility(View.VISIBLE);
+        }
+
+        if (number >= 3) {
+            mDice3.setVisibility(View.VISIBLE);
+        }
+
+        if (number >= 4) {
+            mDice4.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void clickOnBoard(View view) {
+        dropDices(true);
+    }
+
+    public void clickNumberButton(View view) {
+        // Получаем номер кнопки
+        Button button = (Button) view;
+        mNumber = Integer.parseInt(button.getText().toString());
+
+        // Выделяем кнопку и готовим доску
+        selectButton(button);
+        prepareBoard(mNumber);
+    }
+
+    private void selectButton(Button button) {
+        clearButtonSelection();
+        button.setSelected(true);
+    }
+
+    private void selectButton(int number) {
+        clearButtonSelection();
+
+        if (number == 1) {
+            mButton1.setSelected(true);
+        } else if (number == 2) {
+            mButton2.setSelected(true);
+        } else if (number == 3) {
+            mButton3.setSelected(true);
+        } else if (number == 4) {
+            mButton4.setSelected(true);
+        }
+    }
+
+    private void clearButtonSelection() {
+        mButton1.setSelected(false);
+        mButton2.setSelected(false);
+        mButton3.setSelected(false);
+        mButton4.setSelected(false);
     }
 
     private void dropDices(boolean sound) {
@@ -225,6 +327,14 @@ public class BoardActivity extends AppCompatActivity {
         return false;
     }
 
+    private void moveDice(View view, int x, int y) {
+        // Установка отступов слева и сверху
+        MarginLayoutParams params = (MarginLayoutParams) view.getLayoutParams();
+        params.leftMargin = x;
+        params.topMargin = y;
+        view.setLayoutParams(params);
+    }
+
     private void loadDiceImage(ImageView dice, int number, int view) {
         // Формируем имя картинки с кубиком - dice_1_01
         String diceNumber = String.valueOf(number);
@@ -234,122 +344,6 @@ public class BoardActivity extends AppCompatActivity {
         // Получаем идентификатор и присваиваем картинку
         int diceImageId = getResources().getIdentifier(diceImage, "drawable", getPackageName());
         dice.setImageResource(diceImageId);
-    }
-
-    private void moveDice(View view, int x, int y) {
-        // Установка отступов слева и сверху
-        MarginLayoutParams params = (MarginLayoutParams) view.getLayoutParams();
-        params.leftMargin = x;
-        params.topMargin = y;
-        view.setLayoutParams(params);
-    }
-
-    public int getStatusBarHeight() {
-        // Получение высоты статус-бара
-        int statusBarHeight = 0;
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
-        }
-        return statusBarHeight;
-    }
-
-    private void prepareBoard(int number) {
-        // Сначала скрываем все кубики
-        mDice2.setVisibility(View.INVISIBLE);
-        mDice3.setVisibility(View.INVISIBLE);
-        mDice4.setVisibility(View.INVISIBLE);
-
-        // Показываем нужное количество кубиков
-        if (number >= 2) {
-            mDice2.setVisibility(View.VISIBLE);
-        }
-
-        if (number >= 3) {
-            mDice3.setVisibility(View.VISIBLE);
-        }
-
-        if (number >= 4) {
-            mDice4.setVisibility(View.VISIBLE);
-        }
-    }
-
-    public void clickOnBoard(View view) {
-        dropDices(true);
-    }
-
-    public void selectNumberCubes(View view) {
-        // Получаем номер кнопки
-        Button button = (Button) view;
-        mNumber = Integer.parseInt(button.getText().toString());
-
-        // Выделяем кнопку и готовим доску
-        selectButton(button);
-        prepareBoard(mNumber);
-    }
-
-    private void selectButton(Button button) {
-        clearButtonSelection();
-        button.setSelected(true);
-    }
-
-    private void selectButton(int number) {
-        clearButtonSelection();
-
-        if (number == 1) {
-            mButton1.setSelected(true);
-        } else if (number == 2) {
-            mButton2.setSelected(true);
-        } else if (number == 3) {
-            mButton3.setSelected(true);
-        } else if (number == 4) {
-            mButton4.setSelected(true);
-        }
-    }
-
-    private void clearButtonSelection() {
-        mButton1.setSelected(false);
-        mButton2.setSelected(false);
-        mButton3.setSelected(false);
-        mButton4.setSelected(false);
-    }
-
-    @SuppressWarnings("deprecation")
-    private void createSoundPool() {
-        if (mSoundPool == null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                // Создаем SoundPool для Android API 21 и выше
-                AudioAttributes attributes = new AudioAttributes.Builder()
-                        .setUsage(AudioAttributes.USAGE_GAME)
-                        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                        .build();
-
-                mSoundPool = new SoundPool.Builder()
-                        .setAudioAttributes(attributes)
-                        .build();
-            } else {
-                // Создаем SoundPool для старых версий Android
-                mSoundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
-            }
-
-            Log.d("myLog", "Создали SoundPool");
-
-            // Получаем id звуковых файлов
-            loadIdSounds();
-        }
-    }
-
-    private void loadIdSounds() {
-        mRollDiceSound = mSoundPool.load(this, R.raw.roll_dice, 1);
-
-        Log.d("myLog", "Получили id звуковых файлов");
-    }
-
-    private void clearSoundPool() {
-        mSoundPool.release();
-        mSoundPool = null;
-
-        Log.d("myLog", "SoundPool очищен");
     }
 
     private int convertDpToPx(int dp) {
